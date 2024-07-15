e78f3a1194408a417926a82a059eda51b43a6680
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2024-07-15 10:59:36 +0300

    Update index.js
    20

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const button = document.querySelector('.button');
 const rundomNumber = document.querySelector('.number');
 const retunnRandomNumber = () => {
   let number;
-  number = Math.round(Math.random() * (10 - 1) + 1);
+  number = Math.round(Math.random() * (20 - 1) + 1);
   rundomNumber.textContent = `Білет №: ${number}`;
 }
 
